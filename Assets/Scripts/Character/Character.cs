69a16f5fcf1fa333a702631385fd9e361cd6f0cd
lucrybpin <lucspinto@outlook.com>
2024-03-25 01:59:18 -0300

    character animations (idle and walking)

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -6,17 +6,21 @@ namespace TopDownAdventure
     public class Character : MonoBehaviour
     {
         [SerializeField] private CharacterMovement _characterMovement;
+        [SerializeField] private CharacterAnimation _characterAnimation;
         [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Animator _animator;
 
         private void Awake()
         {
             _characterMovement = new CharacterMovement(_characterController);
+            _characterAnimation = new CharacterAnimation(_animator);
         }
 
         public void MoveFromInput(Vector2 movementInput)
         {
             float angle = _characterMovement.Rotate(movementInput, Time.deltaTime);
             _characterMovement.Move(movementInput, angle, Time.deltaTime);
+            _characterAnimation.UpdateMovementAnimation(movementInput);
         }
     }
 }
---
+++ Assets/Scripts/Character/CharacterAnimation.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace TopDownAdventure
+{
+    [System.Serializable]
+    public class CharacterAnimation
+    {
+        [SerializeField] private Animator _animator;
+        
+        public CharacterAnimation(Animator animator)
+        {
+            this._animator = animator;
+        }
+
+        public void UpdateMovementAnimation(Vector2 movementInput)
+        {
+            bool isMoving = false;
+            if (movementInput.magnitude > 0)
+            {
+                isMoving = true;
+            }
+            _animator.SetBool("isMoving", isMoving);
+        }
+
+    }
+}
